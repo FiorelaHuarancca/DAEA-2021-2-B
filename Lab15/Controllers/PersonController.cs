8450b87316852a53e64f68780306304c186a10e7
Jeydi Fiorela Huarancca Pacheco <jeydi.huarancca@tecsup.edu.pe>
2021-11-26 22:23:58 -0500

    Labo 15

---
+++ Lab15/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab15.Models;
+using Microsoft.Reporting.WebForms;
+using System.IO;
+
+using System.Web.Helpers;
+using System.Collections;
+
+namespace Lab15.Controllers
+{
+    public class PersonController : Controller
+    {
+        private SchoolEntities _contexto;
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
