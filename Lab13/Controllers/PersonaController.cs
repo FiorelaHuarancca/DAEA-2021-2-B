c542350e850ca5a685463b63c81457ec14067b0c
Jeydi Fiorela Huarancca Pacheco <jeydi.huarancca@tecsup.edu.pe>
2021-12-10 11:17:04 -0500

    acualizacion

--- Lab13/Controllers/PersonaController.cs
+++ Lab13/Controllers/PersonaController.cs
@@ -9,13 +9,14 @@ namespace Lab13.Controllers
 {
     public class PersonaController : Controller
     {
+
         // GET: Persona
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Listar()
+        public ActionResult Listar(string buscarpor, string buscar)
         {
             List<Persona> personas = new List<Persona>();
             personas.Add(new Persona
@@ -36,16 +37,62 @@ namespace Lab13.Controllers
                 FechaNac = Convert.ToDateTime("1995-10-28"),
                 Email = "maria@mail.com"
             });
+
+            if (buscarpor == "Nombre")
+            {
+                return View(personas.Where(x => x.Nombre.StartsWith(buscar) || buscar == null).ToList());
+            }
+            else if (buscarpor == "Apellido")
+            {
+                return View(personas.Where(x => x.Apellido.StartsWith(buscar) || buscar == null).ToList());
+            }
+            else
+            {
+                return View(personas);
+            }
+
+            //return View(personas);
+        }
+
+        public ActionResult Mostrar(int id)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "Av. Evergen 123",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "María",
+                Apellido = "Salas",
+                Direccion = "Av. Progreso 325",
+                FechaNac = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@mail.com"
+            });
+
             personas.Add(new Persona
             {
                 PersonaID = 3,
                 Nombre = "Carlos",
-                Apellido = "Martínez",
-                Direccion = "Av. Los Manzanos 101",
+                Apellido = "Martinez",
+                Direccion = "Av. Los manzanos 3101",
                 FechaNac = Convert.ToDateTime("1982-02-14"),
                 Email = "carlos@mail.com"
             });
-            return View(personas);
+
+            Persona persona = (from p in personas
+                               where p.PersonaID == id
+                               select p).FirstOrDefault();
+
+            return View(persona);
         }
+
     }
 }
